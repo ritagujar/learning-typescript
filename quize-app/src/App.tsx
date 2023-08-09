40cd4c1e1c5c252aee2e4dbbde2eb62728bbd12a
ritagujar <ritagujar14@gmail.com>
2023-08-09 22:35:52 +0530

    Added the basic structure

--- quize-app/src/App.tsx
+++ quize-app/src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import QuestionCard from "./components/QuestionCard";
 
 function App() {
-  return <div className="App">Quiz</div>;
+  const startTivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
 }
 
 export default App;
