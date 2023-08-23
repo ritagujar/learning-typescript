45bdfa2bcf524c7c25aaeec0ecaa0494622f3015
ritagujar <ritagujar14@gmail.com>
2023-08-24 01:03:57 +0530

    Added the functionality of next question

--- quize-app/src/App.tsx
+++ quize-app/src/App.tsx
@@ -60,7 +60,16 @@ function App() {
     }
   };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
 
   return (
     <div className="App">
