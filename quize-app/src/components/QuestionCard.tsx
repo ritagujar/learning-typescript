ae1951e0dd747a0eccad9113aafdc03b879103ec
ritagujar <ritagujar14@gmail.com>
2023-08-24 02:12:41 +0530

    Added the Styling to the app

--- quize-app/src/components/QuestionCard.tsx
+++ quize-app/src/components/QuestionCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AnswerObject } from "../App";
+import { ButtonWrapper, Wrapper } from "./QuestionCard.styles";
 
 type Props = {
   question: string;
@@ -19,14 +20,18 @@ const QuestionCard: React.FC<Props> = ({
   totalQuestions,
 }) => {
   return (
-    <div>
+    <Wrapper>
       <p className="number">
         Question: {questionNr} / {totalQuestions}
       </p>
       <p dangerouslySetInnerHTML={{ __html: question }} />
       <div>
         {answers.map((answer) => (
-          <div key={answer}>
+          <ButtonWrapper
+            key={answer}
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+          >
             <button
               disabled={userAnswer ? true : false}
               value={answer}
@@ -34,10 +39,10 @@ const QuestionCard: React.FC<Props> = ({
             >
               <span dangerouslySetInnerHTML={{ __html: answer }} />
             </button>
-          </div>
+          </ButtonWrapper>
         ))}
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
