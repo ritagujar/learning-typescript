c501727cc604cd1b73af83bbf8a6b2d54bb10b2b
ritagujar <ritagujar14@gmail.com>
2023-08-25 23:37:40 +0530

    Created the Handgman Drawing

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,13 +1,34 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWords from "./HangmanWords";
+import Keyboard from "./Keyboard";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
 
+  const [guessedLatter, setGuessedLetter] = useState<string[]>([]);
+
   console.log(wordToGuess);
-  return <h1>Hii</h1>;
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Loss Win</div>
+      <HangmanDrawing />
+      <HangmanWords />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
