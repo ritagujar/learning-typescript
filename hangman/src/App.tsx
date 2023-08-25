aa021114b820c5f9d15d9b8d233c56662d3b9b88
ritagujar <ritagujar14@gmail.com>
2023-08-25 23:13:37 +0530

    Created the list of set of words will be used in game

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  console.log(wordToGuess);
   return <h1>Hii</h1>;
 }
 
